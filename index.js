6e1393bbc17149e1f76b3d4c900953ea2a156b2c
Anjanique <anjaniquemackey@gmail.com>
2022-11-20 14:38:41 -0800

    fixes errors about observer and click method

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const vidPlayer = document.querySelector("#movie_player");
-
 const observer = new MutationObserver((mutations) => {
-    console.log("working");
     for (const mutation of mutations) {
-        if (mutation.target.className == "ytp-ad-skip-button-container") {
-            const adSkipBtn = document.querySelector(".ytp-ad-skip-button.ytp-button");
+        const clickBtn = () => {
+            if (document.querySelector(".ytp-ad-skip-button.ytp-button")) {
+                const adBtn = document.querySelector(".ytp-ad-skip-button.ytp-button");
+                // allows for the user to see that the ad ended
+                setTimeout(() => adBtn.click(), 700);
+            }
+        };
+        mutation.target.className === "ytp-ad-skip-button-container" ? clickBtn() : null;
 
-            // allows for the user to see that the ad ended
-            setTimeout(() => adSkipBtn.click(), 500);
-        }
     }
 
 });
-
-observer.observe(vidPlayer, { attributes: true, childList: true, subtree: true });
+observer.observe(document.body, { attributes: true, childList: true, subtree: true });
